fbcac07c51c0325752e63a5d926b0971b6bd194d
OliverFalden <oliverfalden@gmail.com>
2020-09-17 10:59:36 +0200

    robot virker

--- robot.py
+++ robot.py
@@ -4,7 +4,7 @@ import time
 def status():
     TCP_PORT = 29999
     BUFFER_SIZE = 1024
-    TCP_IP = '10.130.58.13' #INDTAST DEN RIGTIGE IP-ADRESSE HER!
+    TCP_IP = '10.130.58.14' #INDTAST DEN RIGTIGE IP-ADRESSE HER!
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(10)
     try:
@@ -22,7 +22,7 @@ def status():
 def perform_task(cmd):
     TCP_PORT = 29999
     BUFFER_SIZE = 1024
-    TCP_IP = '10.130.58.13' #INDTAST DEN RIGTIGE IP-ADRESSE HER!
+    TCP_IP = '10.130.58.14' #INDTAST DEN RIGTIGE IP-ADRESSE HER!
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(10)
     try:
@@ -39,4 +39,27 @@ def perform_task(cmd):
     response = s.recv(BUFFER_SIZE)
     s.close()
 
-perform_task("lego")
+
+def stop_task(cmd):
+    TCP_PORT = 29999
+    BUFFER_SIZE = 1024
+    TCP_IP = '10.130.58.14' #INDTAST DEN RIGTIGE IP-ADRESSE HER!
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(10)
+    try:
+        s.connect((TCP_IP, TCP_PORT))
+        response = s.recv(BUFFER_SIZE)
+    except socket.error:
+        print("Socket error")
+        s.close()
+
+    st = "load /programs/{}.urp\n".format(cmd)
+    s.send(bytearray(st,'utf8'))
+    response = s.recv(BUFFER_SIZE)
+    s.send(b"stop\n")
+    response = s.recv(BUFFER_SIZE)
+    s.close()
+
+#perform_task("roed_klods")
+
+stop_task("roed_klods")
